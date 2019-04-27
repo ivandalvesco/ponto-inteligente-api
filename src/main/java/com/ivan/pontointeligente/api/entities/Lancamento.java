05cbbb3c6936f88d425aead6ef08443d8561429c
ivandalvesco <ivan.dal.vesco@gmail.com>
2019-04-27 16:24:17 -0300

    Criação de Entidades

---
+++ src/main/java/com/ivan/pontointeligente/api/entities/Lancamento.java
@@ -0,0 +1,91 @@
+package com.ivan.pontointeligente.api.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.ivan.pontointeligente.api.enums.TipoEnum;
+
+@Entity
+@Table(name = "lancamento")
+public class Lancamento implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name = "data", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data;
+	@Column(name = "descricao", nullable = false)
+	private String descricao;
+	@Column(name = "localizacao", nullable = true)
+	private String localizacao;
+	@Column(name = "data_criacao", nullable = false)
+	private Date dataCriacao;
+	@Column(name = "data_atualizacao", nullable = false)
+	private Date dataAtualizacao;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tipo", nullable = false)
+	private TipoEnum tipo;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Funcionario funcionario;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getLocalizacao() {
+		return localizacao;
+	}
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+	public TipoEnum getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoEnum tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
